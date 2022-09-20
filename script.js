7aa93538f11ff3b8e508d6caf2f9b7b2499a89b3
grada92 <danielegradassai@gmail.com>
2022-09-20 14:54:58 +0200

    import cdn

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('JS OK')
+
+const app = new Vue (
+    {
+        el: '#app',
+        data: {
+            emails : "",
+        },
+        mounted(){
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (response) => {
+                const result = response.data
+            })
+        }
+    }
+)
