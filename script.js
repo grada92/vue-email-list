e990fe0be77191b89d0c5ac5783aacf06b0fa992
grada92 <danielegradassai@gmail.com>
2022-09-20 15:38:17 +0200

    generate random email

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ const app = new Vue (
     {
         el: '#app',
         data: {
-            emails : "",
+            emails : [],
         },
-        mounted(){
+        mounted() {
+            
+            
+            for(let i = 0; i < 10; i++)
             axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then( (response) => {
+            .then((response) => {
                 const result = response.data
-            })
+                console.log(result.response)
+                
+                this.emails.push(result.response)
+            })  
         }
     }
-)
+);
